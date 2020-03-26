abf48e265ac6a27da3ed6b28179e6a31da64cbb4
Elise St Hilaire <eliseylenore@gmail.com>
2020-03-26 16:19:03 -0700

    Delete hello code

--- server.js
+++ server.js
@@ -25,22 +25,18 @@ mongoose
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
-//First route
-app.get("/", (req, res) => {
-  res.send("Hlo");
-});
-
 //Use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 5555;
